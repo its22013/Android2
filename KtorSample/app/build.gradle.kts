ff285088b04a3a83859904da4a735d688791ff6e
s22013 <s22013@std.it-college.ac.jp>
2023-10-11 09:38:15 +0900

    11章終わり

--- KtorSample/app/build.gradle.kts
+++ KtorSample/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "jp.ac.it_college.std.s22013.ktorsample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "jp.ac.it_college.std.s22013.ktorsample"
@@ -45,14 +45,16 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("io.ktor:ktor-client-core:2.3.5")
     implementation("io.ktor:ktor-client-cio:2.3.5")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.5")
-    implementation("io.ktor:ktor-serialization:2.3.5")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- KtorSample/app/src/main/java/jp/ac/it_college/std/s22013/ktorsample/MainActivity.kt
+++ KtorSample/app/src/main/java/jp/ac/it_college/std/s22013/ktorsample/MainActivity.kt
@@ -1,9 +1,41 @@
 package jp.ac.it_college.std.s22013.ktorsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.UiThread
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.cio.endpoint
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.url
+import io.ktor.serialization.kotlinx.json.json
 import jp.ac.it_college.std.s22013.ktorsample.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s22013.ktorsample.model.WeatherInfo
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+
+private val ktorClient = HttpClient(CIO) {
+    engine {
+        endpoint {
+            connectTimeout = 5000
+            requestTimeout = 5000
+            socketTimeout = 5000
+        }
+    }
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }
+        )
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -11,8 +43,8 @@ class MainActivity : AppCompatActivity() {
             "https://api.openweathermap.org/data/2.5/weather?lang=ja"
         private const val APP_ID = BuildConfig.APP_ID
     }
-    private lateinit var binding: ActivityMainBinding
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,9 +52,32 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvCityList.apply {
             adapter = CityAdapter {
-
+                getWeatherInfo(it.q)
             }
             layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    @UiThread
+    private fun getWeatherInfo(q: String) {
+        lifecycleScope.launch {
+            // データ取得部分
+            val url = "$WEATHER_INFO_URL&q=$q&appid=$APP_ID"
+            val result = ktorClient.get {
+                url(url)
+            }.body<WeatherInfo>()
+
+            // 取得したデータを UI に反映
+            result.run {
+                binding.tvWeatherTelop.text = getString(R.string.tv_telop, cityName)
+                binding.tvWeatherDesc.text = getString(
+                    R.string.tv_desc,
+                    weather[0].description,
+                    coordinates.longitude,
+                    coordinates.latitude
+                )
+            }
+
         }
     }
 }
