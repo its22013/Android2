f60a1b1903b5098668272bdf566bc07afca16608
s22013 <s22013@std.it-college.ac.jp>
2023-11-08 17:02:30 +0900

    押してランダムでポケモンでるまでできた

--- PokeApi/app/src/main/java/jp/ac/it_college/std/s22013/poke_api/MainActivity.kt
+++ PokeApi/app/src/main/java/jp/ac/it_college/std/s22013/poke_api/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.ac.it_college.std.s22013.poke_api
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,16 +11,12 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import jp.ac.it_college.std.s22013.poke_api.api.Games
 import jp.ac.it_college.std.s22013.poke_api.ui.theme.PokeApiPrototypeTheme
 import kotlinx.coroutines.launch
@@ -37,10 +34,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScene(modifier: Modifier = Modifier) {
-    var resultText by remember {
-        mutableStateOf("結果表示")
+    var imageUrl by remember {
+        mutableStateOf("") // 画像のURLを保持する変数
     }
-    // Composable な関数内でコルーチンを使用するためのコルーチンスコープ
     val scope = rememberCoroutineScope()
 
     Surface(modifier) {
@@ -51,19 +47,29 @@ fun MainScene(modifier: Modifier = Modifier) {
                 modifier = Modifier.padding(vertical = 8.dp),
                 onClick = {
                     scope.launch {
-                        resultText = Games.getGenerations().toString()
+                        imageUrl = Games.getPokemonImage() // ポケモンの画像URLを取得
                     }
                 }
             ) {
-                Text(text = "API Test")
+                Text(text = "押してみろ！")
             }
-            Surface(
-                color = MaterialTheme.colorScheme.secondary,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f)
-            ) {
-                Text(text = resultText)
+
+            if (imageUrl.isNotEmpty()) {
+                // 画像URLがある場合、Imageコンポーネントで画像を表示
+                Image(
+                    painter = rememberImagePainter(
+                        data = imageUrl,
+                        builder = {
+                            crossfade(true)
+                        }
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            } else {
+                // 画像URLがまだ取得されていない場合、何も表示しない
+                Text(text = "No image loaded yet")
             }
         }
     }
--- PokeApi/app/src/main/java/jp/ac/it_college/std/s22013/poke_api/api/Games.kt
+++ PokeApi/app/src/main/java/jp/ac/it_college/std/s22013/poke_api/api/Games.kt
@@ -9,11 +9,19 @@ import jp.ac.it_college.std.s22013.poke_api.model.NamedApiResourceList
  * いま時点では、世代の一覧を取る機能のみ
  */
 object Games {
-    /**
-     * /generation エンドポイントへパラメータなしだと
-     * [NamedApiResourceList] 型で取得できる。
-     */
-    public suspend fun getGenerations(): NamedApiResourceList {
-        return ApiClient.get("/generation").body()
+        suspend fun getPokemonImage(): String {
+            // PokeAPIからポケモンの画像URLを取得するコード
+            val pokemonId = (1..1000).random() // 1から898の間でランダムなポケモンIDを取得
+
+            // 例として、ポケモンのIDに基づいて画像URLを取得（仮定のURL）
+            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokemonId.png"
+        }
+
+        /**
+         * /generation エンドポイントへパラメータなしだと
+         * [NamedApiResourceList] 型で取得できる。
+         */
+        public suspend fun getGenerations(): NamedApiResourceList {
+            return ApiClient.get("/generation").body()
+        }
     }
-}
