9d561802b2d4ce01d8d8ae3860eddc989c14535d
s22013 <s22013@std.it-college.ac.jp>
2023-11-08 11:32:35 +0900

    一時的な変更コミット

---
+++ PokeApi/app/src/main/java/jp/ac/it_college/std/s22013/poke_api/api/ApiClint.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s22013.poke_api.api
+
+//object ApiClint {
+//    private val ktor = HttpClinet(CIO) {
+//        engine {
+//            endpoint {
+//
+//            }
+//        }
+//    }
+//}
